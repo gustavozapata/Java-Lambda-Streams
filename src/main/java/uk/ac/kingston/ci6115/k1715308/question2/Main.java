7ce4af3a61170273f648a6bffbf748735496f5ee
Gustavo Zapata <k1715308@kingston.ac.uk>
2021-02-03 15:01:41 +0000

    add streams calculations in functional interface

--- src/main/java/uk/ac/kingston/ci6115/k1715308/question2/Main.java
+++ src/main/java/uk/ac/kingston/ci6115/k1715308/question2/Main.java
@@ -35,13 +35,11 @@ public class Main {
     public static void main(String[] args) {
         List<List<String>> Numbers = readData();
         List<Double> NumberList = new ArrayList<>();
-        List<Integer> NumberListInt = new ArrayList<>();
         
-        //clean up the List: single dimensional list and convert values from String to Integer
+        //clean up the List (flatten to single dimensional list) and convert values from String to Double
         for(List<String> number : Numbers){
             for(String num : number){
                 NumberList.add(Double.parseDouble(num));
-                NumberListInt.add(Integer.parseInt(num));
             }
         }
         System.out.println(NumberList); //[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
@@ -70,28 +68,66 @@ public class Main {
         
         
         //c) Java Streams - Mean and Standard Deviation
-        //Mark: you can link up more than one intermediate operation so the results of one feed into the next.
         /*
-        Count:              15
-        Sum:                1596
-        Mean:               106.4
-        Variance:           28817.04
-        Standard Deviation: 169.75582464234
+        NumberList: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
+        Count:              15              | number of elements in the list
+        Sum:                1596            | sum of all numbers
+        Mean:               106.4           | this is the average of the numbers
+        Variance:           28817.04        | the average of the squared differences from the Mean
+        Standard Deviation: 169.75582464234 | it is the square root of the Variance
         */
         double sum = NumberList.stream().mapToDouble(num -> num).sum();
         double mean = NumberList.stream().mapToDouble(num -> num).average().orElse(0.0);
         double variance = NumberList.stream().map(num -> num - mean).map(num -> num*num).mapToDouble(num -> num).average().getAsDouble();
-        
-        int sumInt = NumberListInt.stream().reduce(0, (n1, n2) -> n1 + n2);
-        System.out.println(sumInt);
-        System.out.println(sumInt / NumberListInt.size());
 
         System.out.println(sum);
         System.out.println(mean);
         System.out.println(variance);
         System.out.println(Math.sqrt(variance));
+        
+        
+        // ########################################
+        
+        
+        //Mark: you can link up more than one intermediate operation so the results of one feed into the next
+        double standardDeviation = NumberList.stream().map(num -> num - mean).map(num -> num*num).map(Math::sqrt).mapToDouble(num -> num).average().getAsDouble();
+        
+        SciCalcStream numberListMean = (list) -> list.stream().mapToDouble(num -> num).average().orElse(0.0);
+        SciCalcStream numberListStandardDeviation = (list) -> list.stream().map(num -> num - mean).map(num -> num*num).mapToDouble(num -> num).average().getAsDouble();
+
+        // Variance: the average of the squared differences from the Mean
+        // for each number: subtract the Mean and square the result, then work out the average of those squared differences
+        // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610 | avg: 106.4
+        // -105.4, -105.4, -104.2, -103.2, -101.2, -98.4, -93.4, -85.4, -72.4, -51.4, -17.4, 37.6, 126.6, 270.6, 503.6
+        // 11,109.16, 11,109.16, 10,857.64, 10,650.24, 10,241.44, 9,682.56, 8,723.56, 7,293.16, 5,241.76, 2,641.96, 302.76, 1,413.76, 16,027.56, 73,224.36, 253,612.96
+        // 432,132.04 / 15 = 28,808.8026666 srt() = 169.731560
+        double myMean = NumberList.stream().mapToDouble(num -> num).sum();
+        System.out.println("myMean: " + myMean);
+        
+        System.out.println("Feb 2021");
+        getSciCalcStreamResult(NumberList, numberListMean);
+        getSciCalcStreamResult(NumberList, numberListStandardDeviation);
+        System.out.println(standardDeviation);
+    }
+    public static void NumberListToInt(){
+        List<List<String>> Numbers = readData();
+        List<Integer> NumberListInt = new ArrayList<>();
+        
+        for(List<String> number : Numbers){
+            for(String num : number){
+                NumberListInt.add(Integer.parseInt(num));
+            }
+        }
+        
+        int sumInt = NumberListInt.stream().reduce(0, (n1, n2) -> n1 + n2);
+        System.out.println(sumInt);
+        System.out.println(sumInt / NumberListInt.size());
     }
     
+    public static void getSciCalcStreamResult(List<Double> list, SciCalcStream calc){
+        double result = calc.doubleOperation(list);
+        System.out.println(result);
+    }
     public static void getBasicCalcResult(int n1, int n2, BasicCalc calc) {
         int result = calc.intOperation(n1, n2);
         System.out.println(result);
--- src/main/java/uk/ac/kingston/ci6115/k1715308/question2/SciCalc.java
+++ src/main/java/uk/ac/kingston/ci6115/k1715308/question2/SciCalc.java
@@ -5,6 +5,8 @@
  */
 package uk.ac.kingston.ci6115.k1715308.question2;
 
+import java.util.List;
+
 /**
  *
  * @author k1715308
@@ -16,4 +18,8 @@ public interface SciCalc {
 //returns an array of values e.g. sin, cos, tan
 interface SciCalcMultiple {
     double[] multipleDoubleOperation(double value);
+}
+
+interface SciCalcStream {
+    double doubleOperation(List<Double> list);
 }
